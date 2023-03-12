76a6b667df4acd5ca319609bb28a4f41f2c91fa4
korah91 <joel.upv>
2023-03-12 18:28:02 +0100

    mas o menos

--- Desarrollo App Web Enriquecidas/Laboratorio 7 Sprites/script.js
+++ Desarrollo App Web Enriquecidas/Laboratorio 7 Sprites/script.js
@@ -36,7 +36,7 @@ window.onload = function() {
       }
       
       // La lupa no se puede salir de los bordes de la imagen
-      if (x > img.width - canvasZoom.width) {
+      if (x > img.width - canvasZoom.width  + 27) {
         x = img.width - canvasZoom.width;
         console.log(x, img.width)
       }
@@ -49,26 +49,42 @@ window.onload = function() {
       if (y < 0) {
         y = 0;
       }
-      
+
+      // Se vuelve a pintar el canvas
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.drawImage(img, 0, 0)
+
       // Se crea la lupa
       ctxZoom.fillStyle = "white"
       ctxZoom.fillRect(0,0, canvasZoom.width, canvasZoom.height);
 
       // drawImage(imagen, sourceX, sourceY, sourceWidth, sourceHeigth, destinationX, destinationY, destinationWidth, destinationHeight)
       // destinationWidth y Height redimensiona la imagen al doble de la original
-      ctxZoom.drawImage(canvas, x, y, 50, 50, 0, 0, 100, 100);
+      ctxZoom.drawImage(canvas, x, y, 50, 50, 0, 0, 100,100);
 
-      
+
+      // Se dibuja el delimitador del recinto seleccionado
+      ctx.beginPath()
+      ctx.moveTo(x, y)
+      ctx.lineTo(x, y)
+      ctx.lineTo(x + canvasZoom.width, y)
+      ctx.lineTo(x + canvasZoom.width, y + canvasZoom.height)
+      ctx.lineTo(x, y + canvasZoom.height)      
+      ctx.closePath()
+      ctx.lineWidth = 3
+      ctx.strokeStyle = "green"
+      ctx.stroke();
+
       // Posicion en la que se utiliza la lupa
-      canvasZoom.style.left = x - canvasZoom.width + "px"
-      canvasZoom.style.top = y - canvasZoom.height + "px"
+      canvasZoom.style.left = x  + "px"
+      canvasZoom.style.top = y  + "px"
       
       
       console.log("("+canvasZoom.style.left + ", " +canvasZoom.style.top+")")
       canvasZoom.style.display = "block";
 
-        
-      ctx.fillRect(x)
+      
+      //ctx.fillRect(x)
     });
 }    
 
