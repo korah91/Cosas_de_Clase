26c2179ef2eb6e96b63df42f0712dc2694225d0f
korah91 <joel.upv@gmail.com>
2023-04-18 18:38:07 +0200

    terminado el postis

--- Desarrollo App Web Enriquecidas/Laboratorio 11 Web Storage/postits.js
+++ Desarrollo App Web Enriquecidas/Laboratorio 11 Web Storage/postits.js
@@ -1,9 +1,9 @@
 /* postits.js
  *
  */
-var num_nota = 1
+var num_nota
 // Lleva la cuenta del numero de notas creadas
-var num_notas = 0
+var num_notas
 
 window.onload = init;
 
@@ -17,10 +17,18 @@ function init() {
 	// por cada una de ellas, llamar al método
 	// addStickyToDOM(texto_de_la_nota);
 
+	// Si no hay notas se pone num_notas a 0
+	num_notas = localStorage.getItem("num_notas")
+	if (num_notas == 'null'){
+		num_notas = 0
+	}
+	num_nota = 1
+
 	var button_clear = document.getElementById("clear_button")
 	button_clear.onclick = clearStickyNotes;
 
 	recoverStickyNotes()
+	actualizarEspacioUtilizado();
 }
 
 function createSticky() {
@@ -29,14 +37,9 @@ function createSticky() {
         // crear la nota con nombre postit_X, donde X es un número entero
 	// (postit_1, postit_2, ...)  y guardarla en el localStorage
 	
-	// Se guarda el valor en localStorage
-	localStorage.setItem("postit_" + num_nota, value)
-	num_nota ++
 	num_notas ++
-
-	// Guardo el numero total de notas para luego poder saber cuantas sacar
-	localStorage.setItem('num_notas', num_notas)
 	addStickyToDOM(value);
+	actualizarEspacioUtilizado();
 }
 
 
@@ -48,6 +51,14 @@ function addStickyToDOM(value) {
 	span.innerHTML = value;
 	postit.appendChild(span);
 	stickies.appendChild(postit);
+
+	// Se guarda el valor en localStorage
+	localStorage.setItem("postit_" + num_nota, value)
+	num_nota ++
+	
+
+	// Guardo el numero total de notas para luego poder saber cuantas sacar
+	localStorage.setItem('num_notas', num_notas)
 }
 
 function clearStickyNotes() {
@@ -66,15 +77,40 @@ function clearStickyNotes() {
 	}
 
 	localStorage.clear()
-	
+	num_notas = 0
+	num_nota = 1
+	actualizarEspacioUtilizado()
 }
 
 
 function recoverStickyNotes(){
 
+	// Recupero el total de notas de antes
 	let num_notas_aux = localStorage.getItem('num_notas')
-	for(var i = 0; i < num_notas_aux; i++){
-		value = localStorage.getItem("postit_" + num_notas_aux)
+	console.log("num_notas -> "+num_notas + " num_notas_aux -> "+num_notas_aux)
+
+	// Recupero cada nota que habia antes
+	for(let i = 1; i <= num_notas_aux; i++){
+		value = localStorage.getItem("postit_" + i)
+		console.log("Se recupera postit_" + i + ": " + value);
+		num_nota = i
 		addStickyToDOM(value)
 	}
+}
+
+// Funcion que actualiza el div para que siempre muestre los kilobytes
+function actualizarEspacioUtilizado(){
+	div = document.getElementById("divEspacio");
+
+	espacioTotal = 0
+	
+	// Conseguimos cada par almacenado en localStorage
+	for(let i = 0; i < localStorage.length; i++){	
+		let key = localStorage.key(i);
+		let value = localStorage.getItem(key);
+
+		// Un caracter ocupa 16 bits = 2 bytes. Queremos bytes
+		espacioTotal += (key.length + value.length) * 2
+	}
+	div.innerHTML = 'Espacio utilizado:' + (espacioTotal / 1024).toFixed(2) + " KB"
 }
